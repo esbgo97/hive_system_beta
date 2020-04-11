552174dc4a26a339f90a72c8e99d2ced1ee9252b
=Edward Bustos <esbgo97@gmail.com>
2020-04-11 13:05:14 -0500

    Updates

---
+++ precache-manifest.f12833d9317ffe770ef43ab91c72a71c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d95a356ed96055a47a5c043aac06d745",
+    "url": "/hive-system-beta/index.html"
+  },
+  {
+    "revision": "2939006ede20c83e8c1b",
+    "url": "/hive-system-beta/static/css/main.c6ad1985.chunk.css"
+  },
+  {
+    "revision": "6f0b36d9400b5f5835d9",
+    "url": "/hive-system-beta/static/js/2.acfeec17.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hive-system-beta/static/js/2.acfeec17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2939006ede20c83e8c1b",
+    "url": "/hive-system-beta/static/js/main.c901df2e.chunk.js"
+  },
+  {
+    "revision": "621799d2db84f9b61ae3",
+    "url": "/hive-system-beta/static/js/runtime-main.1477ddca.js"
+  }
+]);
